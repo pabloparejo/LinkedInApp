689dba56839965ea3e82ff634a51779c961592d3
pabloparejo <parejo@iMac-de-parejo.local>
2015-04-28 20:02:37 +0200

    Initial Commit

---
+++ LinkedIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkedIn
+//
+//  Created by parejo on 28/4/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LinkedIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinkedIn
+//
+//  Created by parejo on 28/4/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
